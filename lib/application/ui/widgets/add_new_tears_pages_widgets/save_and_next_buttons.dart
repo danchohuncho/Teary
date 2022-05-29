7fe78027c96345096cd39e00147f275a15cffd4f
danchohuncho <91431560+danchohuncho@users.noreply.github.com>
2022-05-29 18:41:58 +0300

    refactoring widget

--- lib/application/ui/widgets/add_new_tears_pages_widgets/save_and_next_buttons.dart
+++ lib/application/ui/widgets/add_new_tears_pages_widgets/save_and_next_buttons.dart
@@ -4,99 +4,66 @@ import '../../../../resources/strings.dart';
 import '../../themes/app_colors.dart';
 
 
-class BottomBarButtons extends StatelessWidget {
-  const BottomBarButtons({
-    Key? key,
-  }) : super(key: key);
+
+class SaveButton extends StatelessWidget {
+
+  SaveButton({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 90,
-      child: Stack(
-        alignment: Alignment.bottomCenter,
-        fit: StackFit.passthrough,
-        children: <Widget>[
-          Positioned(
-            bottom: 36,
-            child: _SaveButton(),
-          ),
-          Positioned(
-            right: 36,
-            bottom: 36,
-            child: _NextButton(),
-          ),
+      width: 134.0,
+      height: 49.0,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(50),
+        boxShadow: [
+          BoxShadow(
+            color: AppColors.black.withOpacity(0.12),
+            spreadRadius: 0,
+            blurRadius: 8,
+            offset: Offset(0, 4),
+          )
+        ],
+        color: AppColors.white,
+      ),
+      child: Center(
+          child: Text(
+        saveButton,
+        style: TextStyle(
+            fontSize: 14.0,
+            fontWeight: FontWeight.w500,
+            color: AppColors.textBlack),
+      )),
+    );
+  }
+}
+
+class NextButton extends StatelessWidget {
+
+  const NextButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 49.0,
+      height: 49.0,
+      decoration: BoxDecoration(
+        color: AppColors.darkViolet,
+        shape: BoxShape.circle,
+        boxShadow: [
+          BoxShadow(
+            color: AppColors.black.withOpacity(0.25),
+            spreadRadius: 0,
+            blurRadius: 8,
+            offset: Offset(0, 2),
+          )
         ],
       ),
-    );
-  }
-}
-
-
-
-class _SaveButton extends StatelessWidget {
-  const _SaveButton({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {},
-      child: Container(
-        width: 134.0,
-        height: 49.0,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(50),
-          boxShadow: [
-            BoxShadow(
-              color: AppColors.black.withOpacity(0.12),
-              spreadRadius: 0,
-              blurRadius: 8,
-              offset: Offset(0, 4),
-            )
-          ],
-          color: AppColors.white,
-        ),
-        child: Center(
-            child: Text(
-          saveButton,
-          style: TextStyle(
-              fontSize: 14.0,
-              fontWeight: FontWeight.w500,
-              color: AppColors.textBlack),
-        )),
-      ),
-    );
-  }
-}
-
-class _NextButton extends StatelessWidget {
-  const _NextButton({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {},
-      child: Container(
-        width: 49.0,
-        height: 49.0,
-        decoration: BoxDecoration(
-          color: AppColors.darkViolet,
-          shape: BoxShape.circle,
-          boxShadow: [
-            BoxShadow(
-              color: AppColors.black.withOpacity(0.25),
-              spreadRadius: 0,
-              blurRadius: 8,
-              offset: Offset(0, 2),
-            )
-          ],
-        ),
-        child: Center(
-            child: Icon(
-          Icons.arrow_forward_ios_rounded,
-          color: AppColors.white,
-        )),
-      ),
+      child: const Center(
+          child: Icon(
+        Icons.arrow_forward_ios_rounded,
+        color: AppColors.white,
+      )),
     );
   }
 }
